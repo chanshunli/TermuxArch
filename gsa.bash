7d3ddc18fc0061282ab1c53d4afa874170b6a89d
SD Rausty <SDRausty@gmail.com>
2019-06-11 02:53:13 -0400

    Commit 456625239 on 20190611

---
+++ gsa.bash
@@ -0,0 +1,47 @@
+#!/bin/env bash
+# Copyright 2019 (c) all rights reserved 
+# by S D Rausty https://sdrausty.github.io
+#####################################################################
+set -Eeuo pipefail
+shopt -s nullglob globstar
+
+_SGSATRPERROR_() { # Run on script error.
+	local RV="$?"
+	echo "$RV"
+	printf "\\e[?25h\\n\\e[1;48;5;138mBuildAPKs %s ERROR:  Generated script error %s near or at line number %s by \`%s\`!\\e[0m\\n" "gsa.bash" "${3:-VALUE}" "${1:-LINENO}" "${2:-BASH_COMMAND}"
+	exit 179
+}
+
+_SGSATRPEXIT_() { # Run on exit.
+	printf "\\e[?25h\\e[0m"
+	set +Eeuo pipefail 
+	exit
+}
+
+_SGSATRPSIGNAL_() { # Run on signal.
+	local RV="$?"
+	printf "\\e[?25h\\e[1;7;38;5;0mBuildAPKs %s WARNING:  Signal %s received!\\e[0m\\n" "gsa.bash" "$RV"
+ 	exit 178 
+}
+
+_SGSATRPQUIT_() { # Run on quit.
+	local RV="$?"
+	printf "\\e[?25h\\e[1;7;38;5;0mBuildAPKs %s WARNING:  Quit signal %s received!\\e[0m\\n" "gsa.bash" "$RV"
+ 	exit 177 
+}
+
+trap '_SGSATRPERROR_ $LINENO $BASH_COMMAND $?' ERR 
+trap _SGSATRPEXIT_ EXIT
+trap _SGSATRPSIGNAL_ HUP INT TERM 
+trap _SGSATRPQUIT_ QUIT 
+
+_GSA_() {
+	(git submodule add https://github.com/$1 $2) || (echo ; echo "Cannot update $2: continuing...") 
+}
+
+_GSA_ docs sdrausty/docsTermuxArch
+_GSA_ docs/imgs sdrausty/imgsTermuxArch
+_GSA_ gens sdrausty/gensTermuxArch
+_GSA_ scripts sdrausty/scriptsTermuxArch
+
+#EOF
